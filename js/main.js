1f098bb0927401cec074bc9084c6ab621b7683f3
WEBEXZ <audi.icon@gmail.com>
2016-07-29 19:24:04 -0500

    Añadiendo actividades

--- js/main.js
+++ js/main.js
@@ -1,3 +1,17 @@
 new Vue({
-  
+  el: "body",
+  data: {
+      titulo: "Actividades",
+      actividades: [
+        {nombre: "Limpiar Oficina"},
+        {nombre: "Comprar Hojas"}
+      ],
+      nuevaActividad: ''
+  },
+  methods: {
+    guardarActividad: function(actividad) {
+      this.actividades.push({nombre: actividad});
+      this.nuevaActividad = "";
+    }
+  }
 })
